4fb41929ff72666c601fdc82930d2c7ec71e18cc
Albina-star <akhalim1@student.ccp.edu>
2021-10-10 23:36:41 -0400

    MavenProject first commit

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class FizzBuzz {
+
+    // String because the code should return words
+public String[] fizzbuzz(int startNumber, int endNumber) {
+    String [] numbers = new String [endNumber -startNumber + 1];
+    int newNumber = startNumber;
+
+
+
+    for (int i = startNumber - startNumber; i < (endNumber - startNumber + 1); i++) { // startNumber - startNumber gives 0
+        if (newNumber % 15 == 0) {  // 15 = 3 * 5
+            numbers[i] = "FizzBuzz";
+        } else if (newNumber % 3 == 0) {
+            numbers[i] = "Fizz";
+        } else if (newNumber % 5 == 0) {
+            numbers[i] = "Buzz";
+        } else {
+            numbers[i] = String.valueOf(newNumber); // числа в виде стрингов складываются в массив
+        }
+
+
+
+        newNumber += 1;
+        System.out.println(Arrays.toString(numbers));
+    }
+    return numbers;
+
+
+}
+}
---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void testFizzBuzz() {  // @Test always public void
+        int startNumber = 1;
+        int endNumber = 15;
+        String[] expectedResult = {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"};
+
+        FizzBuzz fizzBuzzObject = new FizzBuzz(); // creating object of FizzBuzz.java
+       String [] actualResult = fizzBuzzObject.fizzbuzz(startNumber, endNumber);
+
+        Assert.assertArrayEquals(expectedResult, actualResult);
+        //Assert.assertNotEquals(expectedResult, actualResult); // negative test, we put wrong data
+    }
+}
